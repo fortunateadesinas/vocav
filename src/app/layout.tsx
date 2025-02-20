175c4bda1e694f2e9237aadf32f344ff3ccb048c
fortunateadesinas <fortunatetemiloluwa@gmail.com>
2025-02-20 21:23:52 +0100

    Resolved conflicts and updated project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link type="image/png" sizes="16x16" rel="icon" href="./icons8-translator-16.png"></link>
+      {/* eslint-disable @next/next/no-sync-scripts */}
       <script src="./node_modules/preline/dist/preline.js"></script>
+      {/* eslint-enable @next/next/no-sync-scripts */}
       <body className={inter.className}>{children}</body>
     </html>
   );
